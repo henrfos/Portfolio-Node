d938cf0f73b0448ab8583d4bcf2d7ea88c1a476b
henrfos <henrik.fossing17@gmail.com>
2024-04-03 15:05:24 +0200

    add basic routing

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var homeRouter = require('./routes/home');
+var aboutRouter = require('./routes/about');
+var servicesRouter = require('./routes/services');
+var recommendationsRouter = require('./routes/recommendations');
+var portfolioRouter = require('./routes/portfolio');
+var contactRouter = require('./routes/contact');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -24,6 +30,12 @@ app.use(express.static(__dirname + '/node_modules/typed.js/lib'));
 app.use(express.static(__dirname + '/node_modules/bootstrap-icons'));
 
 app.use('/', indexRouter);
+app.use('/home', homeRouter);
+app.use('/about', aboutRouter);
+app.use('/services', servicesRouter);
+app.use('/recommendations', recommendationsRouter);
+app.use('/portfolio', portfolioRouter);
+app.use('/contact', contactRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
